be4134c65e79b5cf0e1b4a8ff3022566afd752b0
Lukáš Píšek <lukin.pisek@email.cz>
2023-01-15 16:28:44 +0100

    Dice done

---
+++ DiceConsoleSim/Program.cs
@@ -0,0 +1,23 @@
+﻿while(true)
+{
+    Console.Write("Input min and max (input just one number to go from 1): ");
+    string[] inputArr;
+    inputArr = (Console.ReadLine() + " 1").Split(" ");
+    int minInt = 1, maxInt = 1;
+
+    while (!int.TryParse(inputArr[0], out maxInt) || inputArr.Length > 3 || !int.TryParse(inputArr[1], out minInt))
+    {
+        Console.Write("Input one or two integer numbers seperated by space: ");
+        inputArr = (Console.ReadLine() + " 1").Split(" ") ;
+    }
+
+
+    if (inputArr.Length == 3 && minInt > maxInt)
+    {
+        int t = minInt;
+        minInt = maxInt;
+        maxInt = t;
+    }
+
+    Console.WriteLine($"\nChosen number between {minInt} and {maxInt} is {new Random().Next(minInt, maxInt + 1)}");
+}
